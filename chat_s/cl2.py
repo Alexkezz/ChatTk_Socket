fa010c684e8c02fe45bd63ad37ce39d4c3bde8ba
Alexkezz <alex391969@gmail.com>
2021-12-05 10:37:12 +0100

    Perfect

--- chat_s/cl2.py
+++ chat_s/cl2.py
@@ -3,6 +3,32 @@ from PIL import ImageTk, Image
 import socket
 import threading
 
+#LOGIN---------------------------------------------------------
+ventana1 = Tk()
+ventana1.title("Login")
+ventana1.geometry("400x170")
+ventana1.resizable(0, 0)
+ventana1.config(bg="grey")
+nombre_usuario = ""
+def enviar_informacion(mensaje):
+    global nombre_usuario
+    nombre_usuario = mensaje
+    nombre_usuario = nombre_usuario.strip()
+    ventana1.destroy()
+    
+texto = Label(ventana1, text="INTRODUCE TU NOMBRE DE USUARIO", font=("Calibri", 18), bg="grey")
+texto.pack(pady=10)
+
+pregunta = Entry(ventana1, font=("Calibri", 18))
+pregunta.pack(pady=10)
+
+boton_send = Button(ventana1, text="Enviar", font=("Calibri", 18), command=lambda:enviar_informacion(pregunta.get()))
+boton_send.pack(pady=10)
+
+ventana1.mainloop()
+
+#ROOT----------------------------------------------------------
+
 root = Tk()
 root.title("Chat_Privado")
 root.resizable(0, 0)
@@ -43,10 +69,11 @@ chat_mensaje.config(width=55, height=14, yscrollcommand=scroll_message2.set, sta
 
 
 def escrbir_pantalla_mio(texto_pantalla):
-    mi_cliente.send(texto_pantalla.encode())
+    envair_texto_pantalla2 = "{} >> {}".format(nombre_usuario, texto_pantalla)
+    mi_cliente.send(envair_texto_pantalla2.encode())
     chat_mensaje.config(state=NORMAL)
     inp_message.delete("1.0", "end")
-    chat_mensaje.insert(INSERT, "Tu >>" + texto_pantalla)
+    chat_mensaje.insert(INSERT, "Tu >> " + texto_pantalla)
     chat_mensaje.see(END)
     chat_mensaje.config(state=DISABLED)
     
@@ -66,7 +93,7 @@ def mostrar_mensaje_socket(mss):
     global first_messaje
     chat_mensaje.config(state=NORMAL)
     if first_messaje != 0:
-        chat_mensaje.insert(INSERT, "Amigo >> {}".format(mss))
+        chat_mensaje.insert(INSERT, mss)
     else:
         chat_mensaje.insert(INSERT, "Server >> {}".format(mss))
         first_messaje = 1
@@ -90,15 +117,16 @@ send_button.place(x=455, y=260, width=60, height=40)
 connection = Label(f_portada, text="CHAT CREADO PARA 2SMX", font=("Calibri", 15))
 connection.place(x=85)
  
-image_import = Image.open("./Hermes2.jpg")
-image_import = image_import.resize((412, 555), Image.ANTIALIAS)
+image_import = Image.open("./baixa.jfif")
+image_import = image_import.resize((412, 585), Image.ANTIALIAS)
  
 img_portada = ImageTk.PhotoImage(image_import)
 lbl_img = Label(f_portada, image=img_portada)
-lbl_img.place(y=65)
+lbl_img.place(y=40)
  
 #PORTADA DERECHA--------------------------------
 
 mi_cliente.connect((IP, PORT))
 threading.Thread(target=recive_messaje).start()
 root.mainloop()
+mi_cliente.close()
